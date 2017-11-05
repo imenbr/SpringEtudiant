72d437d25d5faaf57ddcf66c9126e04bf87b3e45
Imen <imen.bradai.ib@gmail.com>
2017-11-05 14:05:07 +0100

    Squelette Spring

---
+++ tp_spring_mvc/src/main/java/tn/enis/TpSpringMvcApplication.java
@@ -0,0 +1,12 @@
+ package tn.enis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpSpringMvcApplication.class, args);
+	}
+}
